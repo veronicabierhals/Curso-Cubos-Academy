69ee5075186765bb8b1ababc568ba8fc9f479212
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-08-18 18:56:01 -0300

    Update carrossel.js

--- Arrays-Loops/AulaPratica/carrossel.js
+++ Arrays-Loops/AulaPratica/carrossel.js
@@ -9,7 +9,19 @@
 //carrossel começa na primeira imagem [0]
 //saida numero entre 0 e 6
 
-let imagens = []
+const imagens = [0, 1, 2, 3, 4, 5, 6]
 
 //foi do índice 0 até o índice 3, voltou para o 2 e foi novamente para o 3 -> print 3
-let cliques = '>>><>' //3x direita, 1x esquerda, 1x direita
+const sequencia = '>>><>' //3x direita, 1x esquerda, 1x direita
+let indiceAtual = 0;
+let indiceParou = 0;
+
+
+for (let item of sequencia){
+    if (item === '>'){
+        indiceAtual = (indiceAtual + 1);
+    } else {
+        indiceAtual = (indiceAtual - 1);
+    } item = indiceAtual
+}
+console.log(indiceAtual)
