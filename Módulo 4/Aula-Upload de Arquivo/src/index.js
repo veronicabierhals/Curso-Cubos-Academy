52aa427b2d2d4ac5eb39e42e973ede7d1c6dc8f7
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-11-15 13:08:58 -0300

    criado endpoint upload-multiple

--- Módulo 4/Aula-Upload de Arquivo/src/index.js
+++ Módulo 4/Aula-Upload de Arquivo/src/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const multer = require("./multer");
-const aws = require('aws-sdk')
+const aws = require("aws-sdk");
 
 const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
 
@@ -18,22 +18,66 @@ const app = express();
 app.use(express.json());
 
 app.post("/upload", multer.single("arquivo"), async (req, res) => {
-  console.log(req.file);
+  const { file } = req;
+  try {
+    const arquivo = await s3
+      .upload({
+        Bucket: process.env.BACKBLAZE_BUCKET,
+        Key: file.originalname,
+        Body: file.buffer,
+        ContentType: file.mimetype,
+      })
+      .promise();
+
+    return res.status(200).json({
+      url: arquivo.Location,
+      path: arquivo.Key,
+    });
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
   return res.json({ mensagem: "tudo certo" });
 });
 
-app.get("/arquivos", async (req, res) => {
+app.post("/upload-multiple", multer.array("arquivo"), async (req, res) => {
+  const { files } = req;
   try {
-    const arquivos = await s3.listObjects({
-        Bucket: process.env.BACKBLAZE_BUCKET,
-      }).promise()
+    const resultado = [];
 
-    return res.json(arquivos);
+    for (const file of files) {
+      const arquivo = await s3
+        .upload({
+          Bucket: process.env.BACKBLAZE_BUCKET,
+          Key: `imagens/multiplas/${file.originalname}`,
+          Body: file.buffer,
+          ContentType: file.mimetype,
+        })
+        .promise();
+
+      resultado.push({
+        url: arquivo.Location,
+        path: arquivo.Key,
+      });
+    }
+
+    return res.status(200).json(resultado);
   } catch (error) {
     res.status(500).json(error.message);
   }
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Servidor iniciado na porta ${process.env.PORT}`);
+app.get("/arquivos", async (req, res) => {
+  try {
+    const arquivos = await s3
+      .listObjects({
+        Bucket: process.env.BACKBLAZE_BUCKET,
+      })
+      .promise();
+
+    return res.json(arquivos.Contents);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 });
+
+app.listen(process.env.PORT);
