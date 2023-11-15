3626f6b7f6052151b5656738490abcb1abaeb507
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-11-15 12:36:52 -0300

    configurados aws e backblaze

--- Módulo 4/Aula-Upload de Arquivo/README.md
+++ Módulo 4/Aula-Upload de Arquivo/README.md
@@ -8,9 +8,27 @@
 * Digital Ocean Objective
 * Wasabi Storage
 * Blackblaze - 10gb armazenamento grátis
+    * https://www.backblaze.com/
 
 ## AWS-SDK
 Conjunto de bibliotecas criado pela Amazon Web Services que facilita a integração com seus serviços.
 
 ### Bucket
 É uma pasta ou recipiente que configuramos nos servidores de armazenamento para armazenar os arquivos.
+
+## Bibliotecas
+### Multer
+https://www.npmjs.com/package/multer
+
+### Backblaze
+https://www.npmjs.com/package/backblaze-b2 <br>
+https://www.backblaze.com/docs/cloud-storage-use-the-aws-sdk-for-javascript-v3-with-backblaze-b2
+
+### AWS-SDK
+https://aws.amazon.com/pt/sdk-for-javascript/ <br>
+https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html <br>
+https://www.npmjs.com/search?q=aws-sdk
+
+```
+npm install aws-sdk
+```
--- Módulo 4/Aula-Upload de Arquivo/src/index.js
+++ Módulo 4/Aula-Upload de Arquivo/src/index.js
@@ -1,12 +1,39 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const multer = require("./multer");
+const aws = require('aws-sdk')
+
+const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
+
+const s3 = new aws.S3({
+  endpoint,
+  credentials: {
+    accessKeyId: process.env.KEY_ID,
+    secretAccessKey: process.env.APP_KEY,
+  },
+});
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", async (req, res) => {
+app.post("/upload", multer.single("arquivo"), async (req, res) => {
+  console.log(req.file);
   return res.json({ mensagem: "tudo certo" });
 });
 
-app.listen(process.env.PORT);
+app.get("/arquivos", async (req, res) => {
+  try {
+    const arquivos = await s3.listObjects({
+        Bucket: process.env.BACKBLAZE_BUCKET,
+      }).promise()
+
+    return res.json(arquivos);
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor iniciado na porta ${process.env.PORT}`);
+});
