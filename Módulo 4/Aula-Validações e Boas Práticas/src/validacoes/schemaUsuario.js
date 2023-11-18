b07bf1e367aaaf8188556d77ea1dbd18ff532d99
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-11-18 11:17:51 -0300

    personalização de erros

--- Módulo 4/Aula-Validações e Boas Práticas/src/validacoes/schemaUsuario.js
+++ Módulo 4/Aula-Validações e Boas Práticas/src/validacoes/schemaUsuario.js
@@ -1,9 +1,22 @@
 const joi = require("joi");
 
 const schemaUsuario = joi.object({
-  nome: joi.string().required(),
-  email: joi.string().email().required(),
-  senha: joi.string().min(5).required(),
+  nome: joi.string().required().messages({
+    "any.required": "O campo nome é obrigatório",
+    "string.empty": "O campo nome é obrigatório"
+  }),
+
+  email: joi.string().email().required().messages({
+    "string.email": "O campo e-mail precisa ter um formato válido",
+    "any.required": "O campo e-mail é obrigatório",
+    "string.empty": "O campo e-mail é obrigatório"
+  }),
+
+  senha: joi.string().min(5).required().messages({
+    "any.required": "O campo senha é obrigatório",
+    "string.empty": "O campo senha é obrigatório",
+    "string.min": "A senha precisa conter no mínimo 5 caracteres",
+  }),
 });
 
 module.exports = schemaUsuario;
