e80e01c8622e0f5005be0bf6ab58c1bd1366bce1
Veronica Bierhals <veronicabierhals1@gmail.com>
2023-11-20 17:17:54 -0300

    rotas refatorado

--- Módulo 4/Aula-Refatoração e Organização/src/controladores/usuarios.js
+++ Módulo 4/Aula-Refatoração e Organização/src/controladores/usuarios.js
@@ -46,4 +46,60 @@ const cadastrarUsuario = async (req, res) => {
   }
 };
 
-module.exports = { cadastrarUsuario };
+const verificarUsuario = async (req, res) => {
+  return res.status(200).json(req.usuario);
+};
+
+const atualizarUsuario = async (req, res) => {
+  let { nome, email, senha, telefone } = req.body;
+  const { id } = req.usuario;
+
+  if (!nome && !email && !senha && !telefone) {
+    return res
+      .status(404)
+      .json("É obrigatório informar ao menos um campo para atualização");
+  }
+
+  try {
+    const usuarioExiste = await bancoDeDados("usuarios").where({ id }).first();
+
+    if (!usuarioExiste) {
+      return res.status(404).json("Usuario não encontrado");
+    }
+
+    if (senha) {
+      senha = await bcrypt.hash(senha, 10);
+    }
+
+    if (email !== req.usuario.email) {
+      const emailUsuarioExiste = await bancoDeDados("usuarios")
+        .where({ email })
+        .first();
+
+      if (emailUsuarioExiste) {
+        res.status(404).json("O Email já existe.");
+        return;
+      }
+    }
+
+    const usuarioAtualizado = await bancoDeDados("usuarios")
+      .where({ id })
+      .update({
+        nome,
+        email,
+        senha,
+        telefone,
+      });
+
+    if (!usuarioAtualizado) {
+      return res.status(400).json("O usuario não foi atualizado");
+    }
+
+    res.status(200).json("Usuario foi atualizado com sucesso.");
+    return;
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = { cadastrarUsuario, verificarUsuario, atualizarUsuario };
--- Módulo 4/Aula-Refatoração e Organização/src/rotas.js
+++ Módulo 4/Aula-Refatoração e Organização/src/rotas.js
@@ -1,72 +1,22 @@
 const express = require("express");
 const rotas = express();
 
-const bcrypt = require("bcrypt");
-const bancoDeDados = require("./conexao");
 const filtroLogin = require("./intermediarios/filtroLogin");
-const { cadastrarUsuario } = require("./controladores/usuarios");
+const {
+  cadastrarUsuario,
+  verificarUsuario,
+  atualizarUsuario,
+} = require("./controladores/usuarios");
 const { login } = require("./controladores/autenticacao");
 
 rotas.post("/usuarios", cadastrarUsuario);
 
-rotas.post("/login", login );
+rotas.post("/login", login);
 
 rotas.use(filtroLogin);
 
-rotas.get("/perfil", async (req, res) => {
-  return res.status(200).json(req.usuario);
-});
+rotas.get("/perfil", verificarUsuario);
 
-rotas.put("/perfil", async (req, res) => {
-  let { nome, email, senha, telefone } = req.body;
-  const { id } = req.usuario;
-
-  if (!nome && !email && !senha && !telefone) {
-    return res
-      .status(404)
-      .json("É obrigatório informar ao menos um campo para atualização");
-  }
-
-  try {
-    const usuarioExiste = await bancoDeDados("usuarios").where({ id }).first();
-
-    if (!usuarioExiste) {
-      return res.status(404).json("Usuario não encontrado");
-    }
-
-    if (senha) {
-      senha = await bcrypt.hash(senha, 10);
-    }
-
-    if (email !== req.usuario.email) {
-      const emailUsuarioExiste = await bancoDeDados("usuarios")
-        .where({ email })
-        .first();
-
-      if (emailUsuarioExiste) {
-        res.status(404).json("O Email já existe.");
-        return;
-      }
-    }
-
-    const usuarioAtualizado = await bancoDeDados("usuarios")
-      .where({ id })
-      .update({
-        nome,
-        email,
-        senha,
-        telefone,
-      });
-
-    if (!usuarioAtualizado) {
-      return res.status(400).json("O usuario não foi atualizado");
-    }
-
-    res.status(200).json("Usuario foi atualizado com sucesso.");
-    return;
-  } catch (error) {
-    return res.status(400).json(error.message);
-  }
-});
+rotas.put("/perfil", atualizarUsuario);
 
 module.exports = rotas;
